af9b0e4341d1fc9f9071170ecc98cf34ab3decbd
lan <lan@landeMacBook-Air.local>
2014-06-11 15:50:57 +0800

    produrce

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -10,11 +10,11 @@ class Userscontroller extends \BaseController {
 	public function getUserLogin()
 	{
 		$account = '11111111111';
-		$password = '11111111111';
+		$password = '1111111111';
 
-		//$result = DB::select('EXEC proc_login ?, ?', array($account, $password));
+		$result = DB::select('EXEC proc_login ?, ?', array($account, $password));
 		//$result = DB::select('select * from zuserlogin');
-		$result = DB::select('EXEC proc_farm_analysis_info ?, ?, ?', array('1', '2014', ''));
+		//$result = DB::select('EXEC proc_farm_analysis_info ?, ?, ?', array('1', '2014', ''));
 		//$result = DB::select('EXEC proc_farm_analysis_info ?, ?, ?', array(, 201, ''));
 		dd($result);
 		//get_type();
