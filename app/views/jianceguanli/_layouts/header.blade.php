7de6f6bd42958fa2f2f53b0ddeb7e49c5e52e707
lan <lan@landeMacBook-Air.local>
2014-07-27 17:08:32 +0800

    modify

--- app/views/jianceguanli/_layouts/header.blade.php
+++ app/views/jianceguanli/_layouts/header.blade.php
@@ -1,4 +1,4 @@
-<div class="header header-zr">
+<div class="header">
 	<div class="cont w85">
 		<div class="welcome">欢迎您，<span class="name">{{Session::get('username')}}</span>！   <a href="{{URL::to('/logout')}}">退出</a></div>
 		<div class="address">区域：{{Session::get('belongArea')}}</div>
